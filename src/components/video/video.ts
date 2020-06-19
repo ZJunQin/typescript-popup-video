555b92891b1515788ec8f14207f75f6227c9b66c
zjunqin <820317059@qq.com>
2020-06-19 17:25:38 +0800

    new version

--- src/components/video/video.ts
+++ src/components/video/video.ts
@@ -94,6 +94,7 @@ class Video implements Icomponent{
 
         videoContent.volume = 0.5;
 
+        //点击视频区域播放/暂停
         videoContent.addEventListener('click', function(){
             if(this.paused){
                 this.play();
@@ -102,31 +103,33 @@ class Video implements Icomponent{
             }
         })
 
+        //点击大播放按钮播放
         videoBigPlay.addEventListener('click', function(){
-            if(videoContent.paused){
-                videoContent.play();
-            }else{
-                videoContent.pause();
-            }
+            videoContent.play();
         })
 
+        //鼠标移入视频区域时控件移入
         this.tempContainer.addEventListener('mouseenter', function(){
             videoControl.style.bottom = 0;
         });
 
+        //鼠标移出视频区域时控件移出
         this.tempContainer.addEventListener('mouseleave', function(){
             videoControl.style.bottom = '-50px';
         })
 
+        //监听视频文件加载前
         videoContent.addEventListener('loadstart', () => {
             videoLoading.style.display = "block";
             videoBigPlay.style.display = "none";
         })
 
+        //监听视频文件等待缓冲中
         videoContent.addEventListener('waiting', () => {
             videoLoading.style.display = "block";
         })
 
+        //监听视频可以播放状态
         videoContent.addEventListener('canplay', () => {
             videoLoading.style.display = "none";
             videoTimes[1].innerHTML = formatTime(videoContent.duration);
@@ -136,20 +139,24 @@ class Video implements Icomponent{
             }
         });
 
+        //监听视频播放中
         videoContent.addEventListener('play', () => {
             videoPlay.className = 'iconfont icon-zanting';
             videoBigPlay.style.display = "none";
             timer = setInterval(playing, 1000);
         });
 
+        //监听视频暂停中
         videoContent.addEventListener('pause', () => {
             videoPlay.className = 'iconfont icon-icon_play';
             videoBigPlay.style.display = "block";
             clearInterval(timer);
         });
 
+        /*监听视频声音变化*/
         videoContent.addEventListener('volumechange', () => {
             if(videoContent.muted == true){
+                videoContent.volume = 0;
                 videoVolProgress[0].style.width = 0;
                 videoVolProgress[1].style.left = 0;
             }else{
@@ -157,8 +164,15 @@ class Video implements Icomponent{
                 videoVolProgress[0].style.width = scale * 100 + "%";
                 videoVolProgress[1].style.left = scale * 100 + '%';
             }
+
+            if(videoContent.volume == 0){
+                videoVolume.className = 'iconfont icon-jingyin';
+            }else{
+                videoVolume.className = 'iconfont icon-shengyin';
+            }
         })
 
+        //点击控件里的播放按钮进行播放/暂停
         videoPlay.addEventListener('click', () => {
             if(videoContent.paused){
                 videoContent.play();
@@ -167,10 +181,12 @@ class Video implements Icomponent{
             }
         })
 
+        //点击全屏按钮进入全屏状态
         videoFull.addEventListener('click', () => {
             videoContent.requestFullscreen();
         })
 
+        //点击视频进度条
         videoProgressGeneral.addEventListener('click', function(ev: MouseEvent){
             let downX = ev.pageX;
             let downL = this.parentNode.parentNode.parentNode.parentNode.offsetLeft;
@@ -186,6 +202,7 @@ class Video implements Icomponent{
             videoContent.currentTime = scale * videoContent.duration;
         })
 
+        //拖动视频进度
         videoProgress[2].addEventListener('mousedown', function(ev: MouseEvent){
             let downX = ev.pageX;
             let downL = this.offsetLeft;
@@ -207,12 +224,14 @@ class Video implements Icomponent{
             ev.preventDefault();
         })
 
+        //点击声音按钮关闭声音
         videoVolume.addEventListener('click', function(){
             videoContent.volume = 0;
             videoVolProgress[0].style.width = 0;
             videoVolProgress[1].style.left = 0;
         })
 
+        //点击声音进度条
         videoVolProgressGeneral.addEventListener('click', function(ev: MouseEvent){
             videoContent.muted = false;
             let downX = ev.pageX;
@@ -229,6 +248,7 @@ class Video implements Icomponent{
             videoContent.volume = scale;
         })
 
+        //拖动声音进度条
         videoVolProgress[1].addEventListener('mousedown', function(ev: MouseEvent){
             videoContent.muted = false;
             let downX = ev.pageX;
@@ -249,6 +269,18 @@ class Video implements Icomponent{
             }
             ev.preventDefault();
         })
+
+        //按空格键进行播放/暂停
+        document.onkeydown = (ev: KeyboardEvent) => {
+            if(ev.keyCode == 32){
+                if(videoContent.paused){
+                    videoContent.play();
+                }else{
+                    videoContent.pause();
+                }
+            }
+            ev.preventDefault();
+        }
 
         function playing(){
             let scale = videoContent.currentTime / videoContent.duration;
